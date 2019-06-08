af8249277b79b51b2fa400cc954aa831b5975728
deadoggy <deadoggy@tongji.edu.cn>
2019-06-08 23:41:59 +0800

    homework

--- 9/homework/lockfreequeue.c
+++ 9/homework/lockfreequeue.c
@@ -106,10 +106,109 @@ void* linkedq_pop(linked_queue* queue){
 * 假设现在队列是(dummy)->(A)
 * 1. T1 进程调用pop，并且在D语句之前被T2抢占, 此时队列是 (dummy(*))->(A)
 * 2. T2 上台运行，同样运行了一个pop并且成功，T2结束运行，(dummy(A))
-* 3. T3 进程接着上台运行，T3 push了一个结点到队尾, 由于共享内存，新push的节点
-* 的地址可能和T2 中free的节点的地址相同，即(dummy(A))->(C)
+* 3. T3 进程接着上台运行，T3 push了一个结点到队尾, 由于共享内存所以(C)的地址
+* 有可能等于(dummy(*)), (dummy(A))->(C)
+* 4. T4 进程上台，调用pop和push并且成功，(dummy(C))->(D)
+* 5. T1 进程上台，因为检测到(dummy(C))和(dummy(*))的地址是一样的，所以CAS中的
+* 比较会成功，T1 会把(D)删除
 */
 
 
 
+// array lock free queue implementation
+
+array_queue init_array_queue(size_t capacity){
+	array_queue retq;
+	retq.capacity = capacity;
+	retq.read_idx = 0;
+	retq.write_idx = 0;
+	retq.max_read_idx = 0;
+	retq.q = (array_queue_node*)malloc((capacity) * sizeof(array_queue_node));
+	return retq;
+}
+
+int get_real_idx(array_queue q, size_t idx){
+	return (idx % q.capacity);
+}
+
+
+/*
+* 逻辑: 先从queue.write_idx处取号， 如果取到的号和当前queue.write_queue的
+* 值一样，那么就将queue.write_idx+1(语句A)， 并且修改自己拿到的号对应位置
+* 的值。如果发现取到的号和queue.write_idx的值不一样，那就重新取号。将取到
+* 号的位置填上数据后，把max_read_idx+1,代表读者可以多读一个数据结点。
+* 
+* 语句A: 如果current_write_idx和queue.write_idx一样，就吧queue.write_idx+1,
+* 代表把current_write_idx位置留给该线程，其他线程要从下一个位置开始push。
+*
+* 语句B: 这步不用做同步操作是因为其他线程只会从queue.write_idx开始写，而此时
+* queue.write_idx比current_write_idx大，而queue.max_read_idx又小于等于
+* current_write_idx,即读者和写者都不会访问current_write_idx的位置，该线程
+* 可以慢慢写这结点的数据
+* 
+* 语句C: 当current_write_idx的数据填好后，更新queue.max_read_idx。这里while循环和
+* CAS的目的是等在自己线程之前的线程更新完成后再更新max_read_idx的值，从而做到了顺序
+* 写。
+*/
+
+bool arrayq_push(array_queue queue, void* data){
+	size_t current_read_idx, current_write_idx;
+	while(true){
+		current_read_idx = queue.read_idx;
+		current_write_idx = queue.write_idx;
+
+		if(get_real_idx(queue, current_write_idx+1)==get_real_idx(queue, current_read_idx)){
+			return false; //full
+		}
+		
+		if(!CAS(&queue.write_idx, current_write_idx, current_write_idx+1)){ // A
+			continue;
+		}
+		queue.q[current_write_idx].data = data; // B
+
+		while(!CAS(&queue.max_read_idx, current_write_idx, current_write_idx+1));// C
+
+		return true;
+	}
+}
+
+
+/*
+* 语句A: 把current_read_idx处的数据取出来
+* 
+* 语句B: 检查current_read_idx的数据是否被其他线程读过，如果没有的话就把queue.read_idx+1
+* 并且返回数据；如果其他线程读取过的话就重新尝试。
+* 
+* 注意，不能把A/B写成:
+* 
+* ...
+* if(!CAS(&queue.read_idx, current_read_idx, current_read_idx+1)){ //C
+* 	continue;
+* }
+* return queue.q[current_read_idx].data; //D
+* ...
+
+* 因为C/D之间时线程可能会被抢占， 其他线程如果读写队列并且套圈又回到current_read_idx
+* 的位置并修改了current_read_idx的值之后，该线程又夺回了CPU，此时返回的值就和被抢占前
+* 的不一样了。
+*/
+void* arrayq_pop(array_queue queue){
+	int current_read_idx;
+	void* ret_data;
+	while(true){
+		current_read_idx = queue.read_idx;
+
+		if(get_real_idx(queue, current_read_idx)==get_real_idx(queue, queue.max_read_idx)){
+			return NULL;
+		}
+		
+		ret_data = queue.q[current_read_idx].data; // A
+
+		if(CAS(&queue.read_idx, current_read_idx, current_read_idx+1)){ //B
+			return ret_data;
+		}
+	}
+	assert(0);
+	return NULL;
+}
 
--- 9/homework/lockfreequeue.h
+++ 9/homework/lockfreequeue.h
@@ -38,8 +38,7 @@ typedef struct _array_queue {
 } array_queue;
 
 array_queue init_array_queue(size_t capacity);
-bool arrayq_isempty(array_queue* queue);
-bool arrayq_push(array_queue* queue, void* data);
-bool arrayq_pop(array_queue* queue);
+bool arrayq_push(array_queue queue, void* data);
+void* arrayq_pop(array_queue queue);
 
 # endif
